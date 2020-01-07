ff0ffdf29b63090c037acc4a7c745b0c5e1ef2d1
Bittner <p.bittner@tu-bs.de>
2020-01-07 11:28:09 +0100

    value 1 for OS Macros such that #if checks also work (instead of only #ifdef)

--- include/polypropylene/definitions/OSDetection.h
+++ include/polypropylene/definitions/OSDetection.h
@@ -6,16 +6,16 @@
 #define POLYPROPYLENE_OSDEFINES_H
 
 #if defined (__linux__) || defined(linux) || defined(LINUX)
-#define PAX_OS_LINUX
+#define PAX_OS_LINUX 1
 
 #elif defined (__unix__) || defined(UNIX)
-#define PAX_OS_UNIX
+#define PAX_OS_UNIX 1
 
 #elif defined(WINDOWS) || defined(WIN32) ||  defined(_WIN32) || defined(__WIN32__)
-#define PAX_OS_WIN
+#define PAX_OS_WIN 1
 
 #elif defined(ANDROID)
-#define PAX_OS_ANDROID
+#define PAX_OS_ANDROID 1
 #endif
 
 #if defined(PAX_OS_WIN) ^ defined(PAX_OS_LINUX) ^ defined(PAX_OS_ANDROID) ^ defined(PAX_OS_UNIX)
