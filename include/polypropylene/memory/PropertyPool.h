e928c0a97a5b7cc512b2d75fadba0012cc2ff403
Bittner <paul.bittner@uni-ulm.de>
2020-03-17 20:27:41 +0100

    removed systems as they had no real benefit for Polypropylene

--- include/polypropylene/memory/PropertyPool.h
+++ include/polypropylene/memory/PropertyPool.h
@@ -6,28 +6,32 @@
 #define POLYPROPYLENE_PROPERTYPOOL_H
 
 #include <polypropylene/memory/AllocationService.h>
-#include "System.h"
-#include "../memory/allocators/PoolAllocator.h"
+#include "paxcore/system/System.h"
+#include "polypropylene/memory/allocators/PoolAllocator.h"
 
 namespace PAX {
+    template<typename PropertyType>
+    struct DefaultChunkValidator {
+        bool isValid(typename PoolAllocator<sizeof(PropertyType)>::MemoryChunk * chunk) {
+            return chunk->allocated;
+        }
+    };
+
     /**
      * Iterator for pool allocators used for PropertyOwningSystems.
      * @tparam PropertyType The type of property this iterator iterates over.
      */
-    template<typename PropertyType>
+    template<typename PropertyType, typename ValidatorType = DefaultChunkValidator<PropertyType>>
     struct PropertyPoolIterator {
     private:
+        ValidatorType validator;
         typename PoolAllocator<sizeof(PropertyType)>::MemoryChunk * current = nullptr;
-
-        inline bool isCurrentValid() {
-            return current->allocated;
-        }
 
     public:
         explicit PropertyPoolIterator(typename PoolAllocator<sizeof(PropertyType)>::MemoryChunk * pos)
-                : current(pos)
+                : current(pos), validator()
         {
-            if (!isCurrentValid()) {
+            if (!validator.isValid(current)) {
                 this->operator++();
             }
         }
@@ -49,7 +53,7 @@ namespace PAX {
         PropertyPoolIterator & operator++() {
             do {
                 ++current;
-            } while (!isCurrentValid());
+            } while (!validator.isValid(current));
 
             return *this;
         }
@@ -65,7 +69,7 @@ namespace PAX {
      * For the specified property type a pool allocator is registered in the allocation service of the corresponding
      * entity.
      */
-    template<class PropertyType>
+    template<class PropertyType, class IteratorType = PropertyPoolIterator<PropertyType>>
     class PropertyPool {
         PoolAllocator<sizeof(PropertyType)> allocator;
 
@@ -77,8 +81,8 @@ namespace PAX {
             allocationService.registerAllocator(typeid(PropertyType), &allocator);
         }
 
-        PropertyPoolIterator<PropertyType> begin() { return allocator.getMemory(); }
-        PropertyPoolIterator<PropertyType> end() { return allocator.getMemory() + allocator.getCapacity(); }
+        IteratorType begin() { return allocator.getMemory(); }
+        IteratorType end() { return allocator.getMemory() + allocator.getCapacity(); }
         //PropertyOwningSystemIterator<PropertyType> begin() const { return Iterator(allocator.getMemory()); }
         //PropertyOwningSystemIterator<PropertyType> end() const { Iterator(allocator.getMemory() + allocator.getCapacity()); }
     };
