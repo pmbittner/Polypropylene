2252f08ffc0d5173b06638877dc26de99e1f4d07
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2021-01-09 16:24:38 +0100

    cpp file for PoolAllocator + some renamings and refactorings

--- include/polypropylene/memory/allocators/PoolAllocator.h
+++ include/polypropylene/memory/allocators/PoolAllocator.h
@@ -14,7 +14,7 @@ namespace PAX {
     /// It can neither grow nor shrink.
     class PoolAllocator : public IAllocator {
     public:
-        static constexpr size_t Initial_Size = 1024;
+        static constexpr size_t DefaultSize = 1024;
         using Index = int32_t;
 
         struct ChunkInfo {
@@ -27,168 +27,56 @@ namespace PAX {
         using memunit = char;
         static constexpr size_t MetaDataSize = sizeof(ChunkInfo);
 
-        size_t ChunkSize() const { return MetaDataSize + elementSize; }
+        /// This is a stack to remember which chunks of memory are free.
+        /// The stack is realised as an array where the top is at the beginning and is moved to the back as elements get popped.
+        /// Hence, when an element is pushed, topIndex is decremented.
+        struct IndexStack {
+            Index * stack;
+            Index topIndex;
 
-        static void * DataOf(ChunkInfo * chunk) {
-            return reinterpret_cast<memunit*>(chunk) + MetaDataSize;
-        }
+            Index pop();
+            void push(Index val);
+        } freeChunks;
 
-        static memunit * fromData(void * data) {
-            // The given pointer points to the data inside a ChunkInfo.
-            // DataOffset describes the byte offset of this data.
-            // Using it allows us to jump to the start of the ChunkInfo.
-            return reinterpret_cast<memunit*>(data) - MetaDataSize;
-        }
-
-        static ChunkInfo* infoFor(memunit * chunk) {
-            return reinterpret_cast<ChunkInfo*>(chunk);
-        }
-
-        /// memory
+        /// Memory
         const Index capacity;
         const size_t elementSize;
         memunit * memory = nullptr;
         int32_t numberOfAllocations = 0;
 
-        size_t MemorySize() const { return capacity * ChunkSize(); }
-
-        Index indexOf(memunit * m) const {
-            return Index((m - memory) / ChunkSize());
-        }
-
-        memunit* memAtIndex(Index index) const {
-            return memory + size_t(index * ChunkSize());
-        }
-
-        /// This is a stack to remember which MemoryChunks are free.
-        /// The stack is realised as an array where the top is at the beginning and is moved to the back as elements get popped.
-        /// Hence, when an element is pushed, topIndex is decremented.
-        struct {
-            Index * stack;
-            Index topIndex;
-
-            Index pop() {
-                Index ret = stack[topIndex];
-                ++topIndex;
-                return ret;
-            }
-
-            void push(Index val) {
-                // Decrement the topIndex to make space for our new val.
-                --topIndex;
-                // Insert such that the stack is sorted and the smallest index stays at the stack top.
-                // Possible future optimisation:
-                // The stack needs not to be sorted. We only need the smallest element of the stack at the front.
-                Index posToInsertFreeIndex = topIndex;
-                while (stack[posToInsertFreeIndex + 1] < val) {
-                    stack[posToInsertFreeIndex] = stack[posToInsertFreeIndex + 1];
-                    ++posToInsertFreeIndex;
-                }
-
-                stack[posToInsertFreeIndex] = val;
-            }
-        } freeChunks;
+        size_t ChunkSize() const;
+        size_t MemorySize() const;
         
-        void clearStack() {
-            // Initialise free chunks stack: All chunks are free now.
-            freeChunks.topIndex = 0;
-            for (Index i = 0; i < capacity; ++i) {
-                freeChunks.stack[i] = i;
-            }
-        }
-        
-        bool freeChunksAvailable() const {
-            return freeChunks.topIndex < capacity;
-        }
+        void clearFreeChunksStack();
+        bool areFreeChunksAvailable() const;
+
+        static void * DataOf(ChunkInfo * chunk);
+        static memunit * FromData(void * data);
+        static ChunkInfo* InfoFor(memunit * chunk);
+        Index indexOf(memunit * m) const;
+        memunit* memAtIndex(Index index) const;
 
     public:
         /// Creates a PoolAllocator of fixedSize.
         /// \param capacity The maximum number of elements that can be allocated simultaneously.
-        explicit PoolAllocator(size_t elementSize, Index capacity = Initial_Size)
-        : elementSize(elementSize), capacity(capacity), numberOfAllocations(0)
-        {
-            memory = new memunit[MemorySize()];
-            freeChunks.stack = new Index[capacity];
-            clear();
-        }
-
-        PoolAllocator(const PAX::PoolAllocator && other)
-        : elementSize(other.elementSize),
-          capacity(other.capacity),
-          memory(other.memory)
-        {
-            freeChunks.stack = other.freeChunks.stack;
-            clear();
-        }
-
+        explicit PoolAllocator(size_t elementSize, Index capacity = DefaultSize);
+        PoolAllocator(const PAX::PoolAllocator && other) noexcept;
         PoolAllocator(const PoolAllocator & other) = delete;
+        
         PoolAllocator & operator=(const PoolAllocator & other) = delete;
 
-        ~PoolAllocator() override {
-            if (numberOfAllocations > 0) {
-                PAX_LOG(PAX::Log::Level::Warn, "Deleting PoolAllocator although there are still " << numberOfAllocations << " elements allocated!");
-            }
+        ~PoolAllocator() override;
 
-            delete[] memory;
-            delete[] freeChunks.stack;
-        }
+        void * allocate() override;
+        void free(void * data) override;
 
-        void * allocate() override {
-            if (freeChunksAvailable()) {
-                ++numberOfAllocations;
-                ChunkInfo * ourChunkInfo = getChunkInfo(freeChunks.pop());
-                ourChunkInfo->allocated = true;
-                return DataOf(ourChunkInfo);
-            } else {
-                PAX_THROW_RUNTIME_ERROR("Memory overflow");
-            }
-        };
-
-        void free(void * data) override {
-            memunit * mem = fromData(data);
-            const Index i = indexOf(mem);
-
-            if (0 <= i && i < capacity) {
-                ChunkInfo * chunkToFree = infoFor(mem);
-                if (chunkToFree->allocated) {
-                    chunkToFree->allocated = false;
-                    --numberOfAllocations;
-                    freeChunks.push(i);
-                } else {
-                    PAX_LOG(PAX::Log::Level::Warn, "Trying to free unallocated memory chunk! aborting...");
-                }
-            } else {
-                // The data seems not to be allocated by us.
-                // We should not throw an exception as this function is likely to be called in destructors.
-                PAX_LOG(PAX::Log::Level::Error, "Given pointer (" << data << ") was not allocated by me!");
-            }
-        };
-
-        void clear() {
-            if (numberOfAllocations == 0) {
-                // Null memory. (Thereby set allocated to false in all chunks).
-                memset(memory, 0, MemorySize());
-                clearStack();
-            } else {
-                PAX_LOG(PAX::Log::Level::Error, "Clearing PoolAllocator although there are still " << numberOfAllocations << " elements allocated");
-            }
-        }
-
-        size_t getAllocationSize() override {
-            return elementSize;
-        }
-
-        ChunkInfo * getChunkInfo(Index index) const {
-            return infoFor(memAtIndex(index));
-        }
-
-        void * getData(Index index) const {
-            return DataOf(getChunkInfo(index));
-        }
-
-        Index getCapacity() {
-            return capacity;
-        }
+        void clear();
+        
+        ChunkInfo * getChunkInfo(Index index) const;
+        void * getData(Index index) const;
+        
+        size_t getAllocationSize() override;
+        Index getCapacity() const;
     };
 }
 
---
+++ src/memory/allocators/PoolAllocator.cpp
@@ -0,0 +1,159 @@
+//
+// Created by Paul Bittner on 09.01.2021.
+//
+
+#include <polypropylene/memory/allocators/PoolAllocator.h>
+
+namespace PAX {
+    size_t PoolAllocator::ChunkSize() const {
+        return MetaDataSize + elementSize;
+    }
+
+    size_t PoolAllocator::MemorySize() const {
+        return capacity * ChunkSize();
+    }
+
+    void * PoolAllocator::DataOf(ChunkInfo *chunk) {
+        return reinterpret_cast<memunit*>(chunk) + MetaDataSize;
+    }
+
+    PoolAllocator::memunit * PoolAllocator::FromData(void *data) {
+        // The given pointer points to the data inside a ChunkInfo.
+        // DataOffset describes the byte offset of this data.
+        // Using it allows us to jump to the start of the ChunkInfo.
+        return reinterpret_cast<memunit*>(data) - MetaDataSize;
+    }
+
+    PoolAllocator::ChunkInfo * PoolAllocator::InfoFor(memunit *chunk) {
+        return reinterpret_cast<ChunkInfo*>(chunk);
+    }
+
+    PoolAllocator::Index PoolAllocator::indexOf(memunit *m) const {
+        return Index((m - memory) / ChunkSize());
+    }
+
+    PoolAllocator::memunit * PoolAllocator::memAtIndex(Index index) const {
+        return memory + size_t(index * ChunkSize());
+    }
+
+    PoolAllocator::PoolAllocator(size_t elementSize, Index capacity) :
+      elementSize(elementSize),
+      capacity(capacity),
+      numberOfAllocations(0),
+      freeChunks()
+    {
+        memory = new memunit[MemorySize()];
+        freeChunks.stack = new Index[capacity];
+        clear();
+    }
+
+    PoolAllocator::PoolAllocator(const PAX::PoolAllocator &&other) noexcept :
+      elementSize(other.elementSize),
+      capacity(other.capacity),
+      numberOfAllocations(other.numberOfAllocations),
+      memory(other.memory),
+      freeChunks()
+    {
+        freeChunks.stack = other.freeChunks.stack;
+        clear();
+    }
+
+    PoolAllocator::Index PoolAllocator::IndexStack::pop() {
+        Index ret = stack[topIndex];
+        ++topIndex;
+        return ret;
+    }
+
+    void PoolAllocator::IndexStack::push(Index val) {
+        // Decrement the topIndex to make space for our new val.
+        --topIndex;
+        // Insert such that the stack is sorted and the smallest index stays at the stack top.
+        // Possible future optimisation:
+        // The stack needs not to be sorted. We only need the smallest element of the stack at the front.
+        Index posToInsertFreeIndex = topIndex;
+        while (stack[posToInsertFreeIndex + 1] < val) {
+            stack[posToInsertFreeIndex] = stack[posToInsertFreeIndex + 1];
+            ++posToInsertFreeIndex;
+        }
+
+        stack[posToInsertFreeIndex] = val;
+    }
+
+    PoolAllocator::~PoolAllocator() {
+        if (numberOfAllocations > 0) {
+            PAX_LOG(PAX::Log::Level::Warn, "Deleting PoolAllocator although there are still " << numberOfAllocations << " elements allocated!");
+        }
+
+        delete[] memory;
+        delete[] freeChunks.stack;
+    }
+
+    void PoolAllocator::clearFreeChunksStack() {
+        // Initialise free chunks stack: All chunks are free now.
+        freeChunks.topIndex = 0;
+        for (Index i = 0; i < capacity; ++i) {
+            freeChunks.stack[i] = i;
+        }
+    }
+
+    bool PoolAllocator::areFreeChunksAvailable() const {
+        return freeChunks.topIndex < capacity;
+    }
+
+    void * PoolAllocator::allocate() {
+        if (areFreeChunksAvailable()) {
+            ++numberOfAllocations;
+            ChunkInfo * ourChunkInfo = getChunkInfo(freeChunks.pop());
+            ourChunkInfo->allocated = true;
+            return DataOf(ourChunkInfo);
+        } else {
+            PAX_THROW_RUNTIME_ERROR("Memory overflow");
+        }
+    }
+
+    void PoolAllocator::free(void *data) {
+        memunit * mem = FromData(data);
+        const Index i = indexOf(mem);
+
+        if (0 <= i && i < capacity) {
+            ChunkInfo * chunkToFree = InfoFor(mem);
+            if (chunkToFree->allocated) {
+                chunkToFree->allocated = false;
+                --numberOfAllocations;
+                freeChunks.push(i);
+            } else {
+                PAX_LOG(PAX::Log::Level::Warn, "Trying to free unallocated memory chunk! aborting...");
+            }
+        } else {
+            // The data seems not to be allocated by us.
+            // We should not throw an exception as this function is likely to be called in destructors.
+            PAX_LOG(PAX::Log::Level::Error, "Given pointer (" << data << ") was not allocated by me!");
+        }
+    }
+
+    void PoolAllocator::clear() {
+        if (numberOfAllocations == 0) {
+            // Null memory. (Thereby set allocated to false in all chunks).
+            memset(memory, 0, MemorySize());
+            clearFreeChunksStack();
+        } else {
+            PAX_LOG(PAX::Log::Level::Error, "Clearing PoolAllocator although there are still " << numberOfAllocations << " elements allocated");
+        }
+    }
+
+    size_t PoolAllocator::getAllocationSize() {
+        return elementSize;
+    }
+
+    PoolAllocator::ChunkInfo * PoolAllocator::getChunkInfo(Index index) const {
+        return InfoFor(memAtIndex(index));
+    }
+
+    void * PoolAllocator::getData(Index index) const {
+        return DataOf(getChunkInfo(index));
+    }
+
+    PoolAllocator::Index PoolAllocator::getCapacity() const {
+        return capacity;
+    }
+}
