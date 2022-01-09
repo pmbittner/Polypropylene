2cf77f45359975097e2ae4eff3fdf5273907d5c7
Paul Bittner <paul.bittner@uni-ulm.de>
2022-01-09 14:45:08 +0100

    set log level to info to also log in release mode

--- examples/pizza/main.cpp
+++ examples/pizza/main.cpp
@@ -44,7 +44,8 @@ int main(int argc, char** argv) {
     using namespace PAX;
     using namespace PAX::Examples;
 
-    PAX_LOG(PAX::Log::Level::Info, "Running Pizza Example");
+    Log::Instance().currentLevel = Log::Level::Info;
+    PAX_LOG(Log::Level::Info, "Running Pizza Example");
 
     /// INITIAL SETUP
     registerPropertyTypes();
